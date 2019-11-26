8a8dd4d7777aeaa8febac758e6d9aee6f871c516
ThaCaper <43811142+ThaCaper@users.noreply.github.com>
2019-11-26 13:57:40 +0100

    Added new Entity
    + Database Context
    + NuGet Packets (Sqlite)

---
+++ SmartPhoneAndCoverWebshop/Entity/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace SmartPhoneShop.Entity
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public bool IsAdmin { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Address Address { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
